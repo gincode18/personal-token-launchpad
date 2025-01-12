e087a604fb41afba8bb906c33163491b5b802722
gincode18 <vishalkamboj9211@gmail.com>
2025-01-12 14:17:25 +0530

    added the main token code 😎

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import {
 } from '@solana/wallet-adapter-react-ui';
 import '@solana/wallet-adapter-react-ui/styles.css';
 import { Home } from "./components/Home";
+import { Toaster } from "@/components/ui/toaster"
 
 export default function App() {
   return (
     <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
       <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
+          <Toaster></Toaster>
           {/* <ModeToggle></ModeToggle> */}
           <Home></Home>
         </WalletModalProvider>
--- src/components/TokenLaunchpad.tsx
+++ src/components/TokenLaunchpad.tsx
@@ -1,28 +1,67 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
+import { MINT_SIZE, TOKEN_2022_PROGRAM_ID, createInitializeMint2Instruction, getMinimumBalanceForRentExemptMint } from "@solana/spl-token"
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js"
 import { CoinsIcon as Coin, Image, Loader2, Package } from 'lucide-react'
 import { useState } from 'react'
 import { HoverButton } from './ui/hover-button'
+import ConfettiSideCannons from "@/lib/ConfettiSideCannons"
+import { useToast } from "@/hooks/use-toast"
 
 export function TokenLaunchpad() {
+  const { toast } = useToast()
   const [name, setName] = useState('')
   const [symbol, setSymbol] = useState('')
   const [imageUrl, setImageUrl] = useState('')
   const [initialSupply, setInitialSupply] = useState('')
   const [isLoading, setIsLoading] = useState(false)
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const { connection } = useConnection();
+  const wallet = useWallet();
+
+  async function createToken() {
+    const mintKeypair = Keypair.generate();
+    const lamports = await getMinimumBalanceForRentExemptMint(connection);
+    if (wallet.publicKey) {
+      const transaction = new Transaction().add(
+        SystemProgram.createAccount({
+          fromPubkey: wallet.publicKey,
+          newAccountPubkey: mintKeypair.publicKey,
+          space: MINT_SIZE,
+          lamports,
+          programId: TOKEN_2022_PROGRAM_ID,
+        }),
+        createInitializeMint2Instruction(mintKeypair.publicKey, 9, wallet.publicKey, wallet.publicKey, TOKEN_2022_PROGRAM_ID)
+      );
+
+      transaction.feePayer = wallet.publicKey;
+      transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+      transaction.partialSign(mintKeypair);
+
+      await wallet.sendTransaction(transaction, connection);
+      console.log(`Token mint created at ${mintKeypair.publicKey.toBase58()}`);
+      ConfettiSideCannons()
+      toast({
+        title: "Token mint Created 🎉",
+        description: `Token mint created at ${mintKeypair.publicKey.toBase58()}`,
+      })
+    }
+  }
+
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
     setIsLoading(true)
+
     // Simulate token creation
-    setTimeout(() => {
-      setIsLoading(false)
-      // Reset form
-      setName('')
-      setSymbol('')
-      setImageUrl('')
-      setInitialSupply('')
-    }, 2000)
+    await createToken();
+
+    setIsLoading(false)
+    setName('')
+    setSymbol('')
+    setImageUrl('')
+    setInitialSupply('')
   }
 
   const isFormValid = name && symbol && imageUrl && initialSupply
